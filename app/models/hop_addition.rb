2bc595eeacc46782486cd668471c9dacc44191dd
Mike Breeze <mike.breeze@rea-group.com>
2010-12-08 00:09:03 +1100

    Made HopAddition act as hop addition. Tidied tables. Added IBU calculation.

--- app/models/hop_addition.rb
+++ app/models/hop_addition.rb
@@ -1,3 +1,16 @@
 class HopAddition < ActiveRecord::Base
+  acts_as_hop_addition
   belongs_to :brew
+
+  def fermentation_volume_litres
+    self.brew.fermentation_volume_litres
+  end
+
+  def kettle_tax_litres
+    self.brew.kettle_tax_litres
+  end
+
+  def original_gravity
+    self.brew.original_gravity
+  end
 end
