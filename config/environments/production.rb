a4008067d9948bb2230d0218c6f83d92765643d1
Mike Breeze <mike.breeze@rea-group.com>
2011-01-10 21:33:54 +1100

    Configured mailer for devise on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Bitchesbrewer::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # devise confirmation mailer
+  config.action_mailer.default_url_options = { :host => 'http://bitchesbrewer.heroku.com/' }
 end
