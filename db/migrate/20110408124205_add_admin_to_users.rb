e7c5faf03b32518ec49ee2815c142a6da59af268
Mike Breeze <mike.breeze@rea-group.com>
2011-04-08 23:27:48 +1000

    Tidied admin migration

--- db/migrate/20110408124205_add_admin_to_users.rb
+++ db/migrate/20110408124205_add_admin_to_users.rb
@@ -1,9 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :admin, :boolean
-
-    sql = 'UPDATE users SET admin = 1 WHERE email = "breezy@binningup.com"'
-    execute(sql)
   end
 
   def self.down
